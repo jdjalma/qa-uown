3dd0e70aa5f624ffac606679e62fe04403fc44ce
jose.mendesdev <Jose.Mendes.gow@uownleasing.com>
2026-04-02 15:25:04 -0300

    CI Test 040226152505

--- docs/taskTestingUown/Ticket371/testInject.py
+++
@@ -1,6 +0,0 @@
-# Cria arquivo vazio em /tmp (Linux/Mac) ou C:\Temp (Windows)
-import os
-try:
-    open('/tmp/testando_injecao_py.txt', 'w').close()
-except:
-    open('C:\\Temp\\testando_injecao_py.txt', 'w').close()
